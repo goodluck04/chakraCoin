e4dc8f7869e5922dfaf4a190e9d40f1cca91f02e
goodluck <goodluckkumar04@gmail.com>
2023-09-16 07:46:17 +0530

    ChakraCoin First Commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,43 @@
+import { Avatar, Box, Stack, Text, VStack } from '@chakra-ui/react'
+import React from 'react';
+import avtar from '../assets/avtar.png'
+
+
+const Footer = () => {
+  return (
+    <Box
+    bgColor={'blackAlpha.900'}
+    color={'whiteAlpha.700'}
+    minH={'48'}
+    px={'16'}
+    py={['16', '8']}
+    >
+
+        <Stack
+        direction={['column', 'row']}
+        h={'full'}
+        alignItems={'center'}
+        >
+            <VStack w={'full'} alignItems={['center', 'flex-start']}>
+                <Text fontWeight={'bold'}>About Us</Text>
+                <Text
+                fontSize={'sm'}
+                letterSpacing={'widest'}
+                textAlign={['center', 'left']}
+                >
+                    We are best crypto trading app in India for real time updates, we provide 
+                    guidance at a very reasonable price.
+                </Text>
+            </VStack>
+            
+            <VStack>
+                <Avatar boxSize={'28'} mt={['4', '0']} src={avtar} />
+                <Text>Our Founder</Text>
+            </VStack>
+ 
+        </Stack>
+    </Box>
+  )
+}
+
+export default Footer
